d84247c64569bf7bd8983c51c6a86bbd329ff221
kadesory92 <sorybana92@gmail.Com>
2025-02-10 19:59:28 +0100

    Mise à jour du fichier vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,12 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), 
     tailwindcss()],
+    build: {
+      outDir: 'dist',
+    },
+    server: {
+      port: 3000,
+    },
 })
 
 
